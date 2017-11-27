e93c4a4adf33ac4f193a4c4fd540042754e6877e
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-11-27 21:17:35 +0200

    Create base model class
    Signed-off-by: Andrew Petrowsky <andrew.petrowsky@outlook.com>

---
+++ src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/Entity.java
@@ -0,0 +1,13 @@
+package com.andrewpetrowski.diploma.bridgelib.Models;
+
+
+import java.util.Date;
+
+public class Entity {
+    public int Id;
+
+    public Date Created_at;
+    public Date Updated_at;
+
+
+}
