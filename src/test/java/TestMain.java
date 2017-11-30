c1632bcc94d9305c88c21e8a1d6c07449fe35907
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-11-30 23:57:02 +0200

    [TESTING] Test POST

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -10,6 +10,27 @@ import org.junit.Test;
 
 public class TestMain {
     @Test
+    public void checkPost() {
+        try {
+            int[] dataf = {7, 1, 1, 8, 5};
+            Headers headers = new Headers();
+            RestApi<DHT11_Data> data = new RestApi<>();
+            data.EnableMapper();
+            DHT11_Data datad = new DHT11_Data(dataf);
+            HttpResponse<JsonNode> res = data.SendPost(datad);
+
+            int i = 0;
+        } catch (UnirestException ex)
+        {
+            String mes = ex.getMessage();
+            int i =0;
+        }
+
+
+
+    }
+
+    @Test
     public void TestParams()
     {
         URLParams params = new URLParams();
@@ -21,4 +42,5 @@ public class TestMain {
 
         int i =0;
     }
+
 }
