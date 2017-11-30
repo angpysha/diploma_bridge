9d6d08d35ce143f3e3fae4a8ab5f6ccbea585ef8
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-11-30 21:38:26 +0200

    RestApi documentation

--- src/main/java/com/andrewpetrowski/diploma/bridgelib/RestApi.java
+++ src/main/java/com/andrewpetrowski/diploma/bridgelib/RestApi.java
@@ -11,9 +11,29 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.Future;
 
+/**
+ * This class provides Rest API support for this project.
+ * There is used <i><b>Unirest Java library</b></i> for simplify HTTP Requests
+ * @param <T> Model class
+ * @author Andrew Petrowsky
+ * @version 0.2
+ */
 public class RestApi<T extends Entity> {
     private final String API_URL = "http://diplomaapi:8080/dhts/add";
 
+    /**
+     * Default class constructor
+     */
+    public RestApi()
+    {
+        this.EnableMapper();
+    }
+
+    /**
+     * <b>Enable object mapper.</b>
+     *
+     * With the help of this method we can map object to class instance and to json
+     */
     public void EnableMapper()
     {
         Unirest.setObjectMapper(new ObjectMapper() {
@@ -43,6 +63,13 @@ public class RestApi<T extends Entity> {
 
     }
 
+    /**
+     * Send POST request
+     * @param url Site URL
+     * @param data Request data
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(String url,T data) throws UnirestException
     {
         return Unirest.post(url)
@@ -50,6 +77,12 @@ public class RestApi<T extends Entity> {
                     .asJson();
     }
 
+    /**
+     * Send POST request
+     * @param data Request data
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(T data) throws UnirestException
     {
         return Unirest.post(API_URL)
@@ -58,6 +91,13 @@ public class RestApi<T extends Entity> {
 
     }
 
+    /**
+     * Send POST request
+     * @param data Request data
+     * @param headers Request headers
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(T data, Headers headers) throws UnirestException
     {
         return Unirest.post(API_URL)
@@ -66,6 +106,14 @@ public class RestApi<T extends Entity> {
                     .asJson();
     }
 
+    /**
+     * Send POST request
+     * @param url Site URL
+     * @param data Request data
+     * @param header Request headers
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(String url,T data, Headers header) throws UnirestException
     {
         return Unirest.post(url)
@@ -74,6 +122,14 @@ public class RestApi<T extends Entity> {
                 .asJson();
     }
 
+    /**
+     * Send POST request
+     * @param url Site URL
+     * @param data Request data
+     * @param headers Request headers
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(String url,T data, Map<String,String> headers) throws UnirestException {
         return Unirest.post(url)
                     .headers(headers)
@@ -81,6 +137,13 @@ public class RestApi<T extends Entity> {
                     .asJson();
     }
 
+    /**
+     * Send POST request
+     * @param data Request data
+     * @param headers Request headers
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(T data, Map<String,String> headers) throws UnirestException
     {
         return  Unirest.post(API_URL)
@@ -89,6 +152,15 @@ public class RestApi<T extends Entity> {
                         .asJson();
     }
 
+    /**
+     * Send POST request
+     * @param url Site URL
+     * @param data Request data
+     * @param headers Request headers
+     * @param params URL parameters
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(String url,T data, Map<String,String> headers,URLParams params) throws UnirestException
     {
         url = url + params.toString();
@@ -98,6 +170,15 @@ public class RestApi<T extends Entity> {
                 .asJson();
     }
 
+    /**
+     * Send POST request
+     * @param url Site URL
+     * @param data Request data
+     * @param headers Request headers
+     * @param params URL parameters
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(String url,T data,Headers headers,URLParams params) throws UnirestException
     {
         url = url + params.toString();
@@ -107,6 +188,14 @@ public class RestApi<T extends Entity> {
                     .asJson();
     }
 
+    /**
+     * Send POST request
+     * @param data Request data
+     * @param headers Request headers
+     * @param urlParams URL parameters
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(T data, Map<String,String> headers, URLParams urlParams) throws UnirestException {
         String newURL = API_URL + urlParams.toString();
 
@@ -116,6 +205,14 @@ public class RestApi<T extends Entity> {
                     .asJson();
     }
 
+    /**
+     * Send POST request
+     * @param data Request data
+     * @param headers Request headers
+     * @param urlParams URL parameters
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public HttpResponse<JsonNode> SendPost(T data, Headers headers, URLParams urlParams) throws UnirestException {
         String newURL = API_URL + urlParams.toString();
 
@@ -125,12 +222,16 @@ public class RestApi<T extends Entity> {
                 .asJson();
     }
 
-
-
+    /**
+     * Send POST request async
+     * @param data Request data
+     * @return Server response as json object
+     * @throws UnirestException
+     */
     public Future<HttpResponse<JsonNode>> SendPostAsync(T data) throws UnirestException {
         return Unirest.post(API_URL)
                       .body(data)
                         .asJsonAsync();
     }
-    
+
 }
