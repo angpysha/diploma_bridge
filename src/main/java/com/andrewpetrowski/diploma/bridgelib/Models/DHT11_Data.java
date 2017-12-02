23fd0138015daff76681b1de8d5a23466cf2534e
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-02 14:12:52 +0200

    Fix error

--- src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/DHT11_Data.java
+++ src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/DHT11_Data.java
@@ -7,5 +7,6 @@ public class DHT11_Data extends Entity {
     public DHT11_Data(int[] data) {
         this.Humidity = (float) ((data[0] << 8) + data[1]) / 10;
         this.Temperature = (float) (((data[2] & 0x7F) << 8) + data[3]) / 10;
+
     }
 }
