ffc0a7f3b1ff8dc6528acf00a3d750afbcc5fea2
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-02 18:58:54 +0200

    Model documentation

--- src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/DHT11_Data.java
+++ src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/DHT11_Data.java
@@ -1,9 +1,21 @@
 package com.andrewpetrowski.diploma.bridgelib.Models;
 
 public class DHT11_Data extends Entity {
+
+    /**
+     * This property shows temperature from sensor
+     */
     public float Temperature;
+
+    /**
+     * This property shows humidity from sensor
+     */
     public float Humidity;
 
+    /**
+     * Default constructor for creating DHT11 sensor data
+     * @param data array of 5 elements, which contains raw sensor data
+     */
     public DHT11_Data(int[] data) {
         this.Humidity = (float) ((data[0] << 8) + data[1]) / 10;
         this.Temperature = (float) (((data[2] & 0x7F) << 8) + data[3]) / 10;
--- src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/Entity.java
+++ src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/Entity.java
@@ -4,9 +4,19 @@ package com.andrewpetrowski.diploma.bridgelib.Models;
 import java.util.Date;
 
 public class Entity {
+    /**
+     * Object's database id
+     */
     public int Id;
 
+    /**
+     * Object's creating date
+     */
     public Date Created_at;
+
+    /**
+     * Object's updating date
+     */
     public Date Updated_at;
 
 
