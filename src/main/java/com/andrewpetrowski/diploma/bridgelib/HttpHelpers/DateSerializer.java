4454066c42d54e58601be0a79aa7028f389a6493
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-16 22:16:03 +0200

    Date serialization to right format

--- src/main/java/com/andrewpetrowski/diploma/bridgelib/HttpHelpers/DateSerializer.java
+++ src/main/java/com/andrewpetrowski/diploma/bridgelib/HttpHelpers/DateSerializer.java
@@ -10,11 +10,27 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * This class is used for serialization date to JSON in right format
+ *
+ * @author Andrew Petrowsky
+ * @version 1.0
+ */
 public class DateSerializer extends JsonSerializer<Date> {
 
-
+    /**
+     * Date format
+     */
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
+    /**
+     * Serialization method override
+     * @param date Date, needed for serialization
+     * @param gen   Json generator instance
+     * @param provider  Serialization provider
+     * @throws IOException
+     * @throws JsonProcessingException
+     */
     @Override
     public void serialize(Date date, JsonGenerator gen, SerializerProvider provider)
             throws IOException, JsonProcessingException {
