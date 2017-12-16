acfd95e652095c9b1785dceb1017492786bfc874
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-16 22:16:22 +0200

    Base class for searching

--- src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/SearchEntity.java
+++ src/main/java/com/andrewpetrowski/diploma/bridgelib/Models/SearchEntity.java
@@ -1,4 +1,10 @@
+/**
+ * This package contains all models required for correct work
+ */
 package com.andrewpetrowski.diploma.bridgelib.Models;
 
+/**
+ * A base class for search
+ */
 public abstract class SearchEntity {
 }
