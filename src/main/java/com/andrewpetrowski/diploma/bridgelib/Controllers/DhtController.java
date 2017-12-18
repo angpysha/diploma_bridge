2e36e0e9e00d0c367f031e1510655b87148a7da1
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-18 22:49:15 +0200

    Add new methods url to dht controller

--- src/main/java/com/andrewpetrowski/diploma/bridgelib/Controllers/DhtController.java
+++ src/main/java/com/andrewpetrowski/diploma/bridgelib/Controllers/DhtController.java
@@ -3,13 +3,22 @@ package com.andrewpetrowski.diploma.bridgelib.Controllers;
 import com.andrewpetrowski.diploma.bridgelib.Models.DHT11_Data;
 import com.andrewpetrowski.diploma.bridgelib.Models.DhtSearch;
 
+/**
+ * This class provides access to DHT11 database table at server
+ * @author Andrew Petrowsky
+ * @version 1.0
+ */
 public class DhtController extends BaseController<DHT11_Data,DhtSearch> {
 
-
+    /**
+     * Dummy class constructor
+     */
     public DhtController() {
         SEARCH_URL = "/dhts/search";
         GET_URL =  "/dhts/get";
         ADD_URL = "/dhts/add";
+        UPDATE_URL="/dhts/update";
+        DELETE_URL="/dhts/delete";
     }
 
 
